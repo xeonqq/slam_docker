75c03864db8ce6af24c8de52030e3559f3fc9703
Qian <xeonqq@gmail.com>
2020-01-11 11:50:06 +0100

    Add parameters to enable debug in clion

--- linux_run.sh
+++ linux_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 xhost +SI:localuser:root
-docker run --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/qq/repo:/workspace -e DISPLAY=$DISPLAY  -p 5000:5000 -p 8888:8888 -it slam:latest /bin/zsh $@
+docker run  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
+--rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/qq/repo:/workspace -e DISPLAY=$DISPLAY  -p 5000:5000 -p 8888:8888 -it slam:latest /bin/zsh $@
