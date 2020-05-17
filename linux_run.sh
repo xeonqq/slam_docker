4ec000832a6ac443a71b2391bbc833459b1b83e2
xeonqq <xeonqq@gmail.com>
2020-05-17 17:30:27 +0200

    Mount local gitconfig into docker

--- linux_run.sh
+++ linux_run.sh
@@ -2,7 +2,7 @@
 
 xhost +SI:localuser:root
 docker run \
---rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/qq/repo:/workspace \
+--rm -v /tmp/.X11-unix:/tmp/.X11-unix -v "$HOME"/.gitconfig:/root/.gitconfig -v "$HOME"/repo:/workspace \
 --privileged \
 -e DISPLAY=$DISPLAY   \
 -p 5000:5000 -p 8888:8888 -it slam:latest /bin/zsh $@
